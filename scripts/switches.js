5a7b1fdff79361d22400bf678123ae82d37920fe
Morgan Vo <morganvo@outlook.com>
2020-08-13 16:46:14 -0400

    implemented switches

---
+++ scripts/switches.js
@@ -0,0 +1,33 @@
+// switches toggle between the display of two things
+// ex: /bio
+function initSwitches() {
+  // collect all switch containers
+  const switchContainers = document.querySelectorAll('.switch-container');
+  for (const switchContainer of switchContainers) {
+    const { target } = switchContainer.dataset;
+
+    // collect all switches of a switch container
+    const switches = document.querySelectorAll(`.switch-${target}`);
+    let i = 0;
+    for (i; i < switches.length; i++) {
+      const switchElm = switches[i],
+        idx = i;
+
+      switchElm.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (idx > 0) {
+          switchContainer.classList.add('active');
+          switches[0].classList.remove('current');
+        }
+        else {
+          switchContainer.classList.remove('active');
+          switches[1].classList.remove('current');
+        }
+
+        switchElm.classList.add('current');
+      });
+    }
+
+  }
+}
