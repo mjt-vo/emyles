028096841dd24b05ffbeb59547a5bd6599025de2
Morgan Vo <morganvo@outlook.com>
2020-08-15 19:36:03 -0400

    toggling drawer nav

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,12 +1,9 @@
 function initNav() {
   const nav = document.querySelector('nav'),
     mainSection = document.querySelector('section.main'),
-    toggles = document.querySelectorAll('.nav-toggle');
+    toggle = document.querySelector('.nav-toggle');
 
-  for (toggle of toggles) {
-    toggle.addEventListener('click', () => {
-      nav.classList.toggle('active');
-      mainSection.classList.toggle('hide');
-    });
-  }
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
 }
