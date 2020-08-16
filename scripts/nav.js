91b0d0a72801848a781340d1989f8f7a3349e3a8
Morgan Vo <morganvo@outlook.com>
2020-08-16 09:45:08 -0400

    added a close/open state

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,20 @@
 function initNav() {
   const nav = document.querySelector('nav'),
-    mainSection = document.querySelector('section.main'),
     toggle = document.querySelector('.nav-toggle');
+
+  let active = false;
 
   toggle.addEventListener('click', () => {
     nav.classList.toggle('active');
+    // closed state
+    if (active) {
+      toggle.innerHTML = 'menu';
+      active = false;
+    }
+    // open state
+    else {
+      toggle.innerHTML = 'close';
+      active = true;
+    }
   });
 }
