5143b6792c0455f5c79598a0c1a146727019704a
Morgan Vo <morganvo@outlook.com>
2020-09-23 12:04:07 -0400

    displaying message when all gigs expire

--- scripts/gigs.js
+++ scripts/gigs.js
@@ -2,9 +2,19 @@
 
 function initGigs() {
   const now = Math.floor(Date.now() / 1000) - 14400,
-    gigs = document.querySelectorAll('.gig');
+    gigs = document.querySelectorAll('.gig'),
+    // message displayed if all gigs have expired
+    noGigs = document.querySelector('.no-gigs');
+
+  let expiredGigs = 0;
 
   for (gig of gigs) {
-    if (gig.dataset.expiration < now) gig.classList.add('expired');
+    if (gig.dataset.expiration < now) {
+      gig.classList.add('expired');
+      expiredGigs++;
+    }
   }
+
+  // show no gigs message
+  if (expiredGigs === gigs.length) noGigs.classList.add('display');
 }
